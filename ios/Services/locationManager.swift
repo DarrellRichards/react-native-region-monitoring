d008f58a7ca6dfc19e6f25c3b0a0675a46d19b8f
Darrell Richards <drichards@valcom.com>
2019-03-06 14:07:38 -0500

    Added a locationManager Tracker

---
+++ ios/Services/locationManager.swift
@@ -0,0 +1,17 @@
+//
+//  locationManager.swift
+//  RNRegionMonitoring
+//
+//  Created by Darrell Richards on 3/6/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class locationTracker {
+    func triggerLocationUpdate(location: CLLocation, url: String) {
+        print(location)
+        print(url)
+    }
+}
